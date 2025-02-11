2bbc92ee2c72e50b14ee9f87303b7a56d678c254
DeepFried-Butter <koradejayesh@gmail.com>
2025-02-11 22:51:47 +0530

    added fruit world files

---
+++ FruitWorld/index.js
@@ -0,0 +1,13 @@
+const mainheading = document.getElementById('main-heading');
+mainheading.textContent = 'Fruit World';
+mainheading.style.color = 'orange';
+
+const header = document.getElementById('header');
+header.style.backgroundColor = 'green';
+header.style.borderBottom = 'solid orange';
+
+const basket = document.getElementById('basket-heading');
+basket.style.color = 'green';
+
+const th = document.getElementById('thanks');
+th.innerHTML='<p>Please visit us again</p>'
